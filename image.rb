3037131c36274434a8b3fc2bfef25f2a3bb4ef11
ishotjr <github@ishotjr.com>
2015-08-16 02:15:47 -0400

    :octocat: :zap: Require rugged, remove Zip prefix

--- image.rb
+++ image.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'gollum-lib'
 require 'tempfile'
-require 'zip/zip'
+require 'zip'
+require 'rugged'
 
 def index ( message=nil )
 	response = File.read(File.join('.', 'index.html'))
@@ -20,7 +21,7 @@ post '/unpack' do
 	@index = Rugged::Index.new
 
 	zip = params[:zip][:tempfile]
-	Zip::ZipFile.open( zip ) { |zipfile| 
+	Zip::File.open( zip ) { |zipfile| 
 		zipfile.each do |f|
 			contents = zipfile.read( f.name )
 			filename = f.name.split( File::Separator ).pop
