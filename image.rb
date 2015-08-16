4e9ea04f05ec3137c9d73b2373f68afdad15b445
ishotjr <github@ishotjr.com>
2015-08-16 02:13:25 -0400

    :open_file_folder: :arrow_up: Add zip upload

--- image.rb
+++ image.rb
@@ -1,9 +1,33 @@
 require 'sinatra'
 require 'gollum-lib'
+require 'tempfile'
+require 'zip/zip'
+
+def index ( message=nil )
+	response = File.read(File.join('.', 'index.html'))
+	response.gsub!( "<!-- message -->\n", "<h2>Received and unpacked #{message}</h2>") if message
+	response
+end
 
 wiki = Gollum::Wiki.new(".")
-set :views, "."
 
-get '/pages' do
-	"All pages: \n" + wiki.pages.collect { |p| p.path }.join{ "\n" }
+get '/' do
+	index()
+end
+
+post '/unpack' do
+	@repo = Rugged::Repository.new('.')
+	@index = Rugged::Index.new
+
+	zip = params[:zip][:tempfile]
+	Zip::ZipFile.open( zip ) { |zipfile| 
+		zipfile.each do |f|
+			contents = zipfile.read( f.name )
+			filename = f.name.split( File::Separator ).pop
+			if contents and filename and filename =~ /(png|jp?g|gif)$/i
+				puts "Writing out: #{filename}"
+			end
+		end
+	}
+	index( params[:zip][:filename] )
 end
